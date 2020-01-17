6a67cc8cb580effef564f6f3d57d5b408326b72f
sshmkht <sasha.mukhtarova1@gmail.com>
2020-01-17 18:20:42 +0300

    modified script

--- src/js/script.js
+++ src/js/script.js
@@ -26,20 +26,17 @@ document.querySelector('.next').addEventListener('click', function () {
 
 	});
 
-  $('.catalog-item__link').each(function(i) {
-    $(this).on('click', function(e) {
-      e.preventDefault();
-      $('catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    })
-  })
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
 
-  $('.catalog-item__back').each(function(i) {
-    $(this).on('click', function(e) {
-      e.preventDefault();
-      $('catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    })
-  })
+  toggleSlide ('.catalog-item__link');
+  toggleSlide ('.catalog-item__back');
 
 })(jQuery);
