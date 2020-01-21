7955c88da3478b545ecfcc971bf8acbcf9ebe42d
sshmkht <sasha.mukhtarova1@gmail.com>
2020-01-21 23:29:21 +0300

    add mailer

--- src/js/script.js
+++ src/js/script.js
@@ -85,8 +85,24 @@ document.querySelector('.next').addEventListener('click', function () {
   validateForms ('#order form');
 
   //phone mask
-$('input[name=phone]').mask("+7 (999) 999-99-99");
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+  //mailer
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
 })(jQuery);
 
 
